422160596b9dff6d059c51f4f0bcb226660e0408
asdfish <lgray3420@gmail.com>
2024-12-24 08:17:01 -0500

    added main branch

---
+++ src/branch/wwm.cpp
@@ -0,0 +1,8 @@
+#include <branch/wwm.hpp>
+
+bool branch_wwm_predicate(const Flags&) {
+  return true;
+}
+int branch_wwm_routine(const Flags&) {
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <branch/base.hpp>
 #include <branch/help.hpp>
 #include <branch/version.hpp>
+#include <branch/wwm.hpp>
 
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
 
@@ -15,6 +16,7 @@ int main(int argc, char* argv[]) {
   static constexpr BranchBase branchs[] = {
     get_branch_help(),
     get_branch_version(),
+    get_branch_wwm(),
   };
   for(std::size_t i = 0; i < ARRAY_LENGTH(branchs); i ++)
     if(branchs[i].predicate(flags))
